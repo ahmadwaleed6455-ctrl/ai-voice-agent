4af169b92f06556e25e5d5e76922ce95bc251b78
Fahad Gul <ahmad.waleed6455@gmail.com>
2026-04-12 18:32:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,18 @@ def index():
 # --- LOGIN PAGE ROUTE ---
 @app.route('/auth')
 def auth_page():
-    return render_template('auth.html')
+    # .env se keys nikal kar dictionary bana rahe hain
+    fb_config = {
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID"),
+        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID")
+    }
+    # Ye data auth.html ko bhej rahe hain
+    return render_template('auth.html', fb_config=fb_config)
 
 # --- LOGIN VERIFICATION ---
 @app.route('/login_success', methods=['POST'])
